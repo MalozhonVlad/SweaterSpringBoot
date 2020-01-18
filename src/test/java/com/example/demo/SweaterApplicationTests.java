5d89bb205c277766aeb87381306631ff75edd026
Vlad <malozhonvlad@gmail.com>
2020-01-18 19:47:25 +0200

    first video

--- src/test/java/com/example/demo/SweaterApplicationTests.java
+++ src/test/java/com/example/demo/SweaterApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.demo;
 
+import static org.hamcrest.Matchers.containsString;
+import com.example.demo.controller.GreetingController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class SweaterApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private GreetingController greetingController;
 
     @Test
     void contextLoads() {
+        assertThat(greetingController).isNotNull();
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("<p>Get your greeting <a href=\"/greeting\">here</a></p>");
+    }
+
+    @Test
+    public void greetingTest() throws Exception {
+        mockMvc.perform(get("/greeting"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World !")));
     }
 
 }
